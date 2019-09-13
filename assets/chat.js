e6dbf84edc0be96a53d6272b6ff068fe502b332d
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-13 17:17:42 +0530

    updated

--- assets/chat.js
+++ assets/chat.js
@@ -13,9 +13,11 @@ $(document).ready(() => {
     path: "/chat/socket"
   });
   client.on("chat-to-client", function(data) {
-    if (data.from !== localStorage.getItem("username")) {
+    if (data.from == localStorage.getItem("username")) {
       console.log("MESSAGE RECIVED" + JSON.stringify(data.message_content));
-      $("#messages").append('<li class="left-chat"><p>' + data + "</p></li>");
+      $("#messages").append(
+        '<li class="left-chat"><p>' + data.message_content + "</p></li>"
+      );
     }
     //to display received message - to push left message
   });
